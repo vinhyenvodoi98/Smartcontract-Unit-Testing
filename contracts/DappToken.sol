d615e37691ea8eab10ea0fa77d884d64537be141
hoang <vinhyenvodoi98@gmail.com>
2019-06-21 14:08:05 +0700

    testing totalSupply

---
+++ contracts/DappToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.5;
+
+contract DappToken {
+    uint256 public totalSupply;
+
+    constructor (uint256 _initialSupply) public {
+        totalSupply = _initialSupply;
+    }
+}
