b8d5ae4fd6fa89c5bb148621c904557bffa0e64f
hoang <vinhyenvodoi98@gmail.com>
2019-06-24 10:36:51 +0700

    Delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,11 +1,9 @@
 pragma solidity ^0.4.5;
 
 contract DappToken {
-  //Name
+
   string public name = "DappToken";
-  //Symbol
   string public symbol = "DAPP";
-  //Standard
   string public standard = "DApp Token v1.0";
 
   uint256 public totalSupply;
@@ -16,7 +14,14 @@ contract DappToken {
     uint256 _value
   );
 
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
   mapping (address => uint256) public balanceOf ;
+  mapping (address => mapping(address => uint256)) public allowance;
 
   constructor (uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -25,14 +30,54 @@ contract DappToken {
 
   // Transfer
   function transfer(address _to, uint256 _value) public returns(bool success) {
-  // Exception if account doesn't have enough
-  require ( balanceOf[msg.sender] >= _value );
-  // Return a boolean
-  balanceOf[msg.sender] -= _value;
-  balanceOf[_to] += _value;
+    require(
+      balanceOf[msg.sender]>=_value,
+      "need balance >= _value"
+    );
 
-  Transfer(msg.sender, _to, _value);
-  // Transfer Event
-  return true;
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+  }
+
+  //Delegated Transfer
+
+  //approve
+  function approve(address _spender, uint256 _value) public returns(bool success){
+
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  //tranferFrom
+  function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+    // require _from has enough tokens
+    require(
+      _value <= balanceOf[_from],
+      "require _from has enough tokens"
+    );
+
+    // require allowance is big enough
+    require(
+      _value <= allowance[_from][msg.sender],
+      "require allowance is big enough"
+    );
+
+    // change the balance
+    balanceOf[_from] -= _value;
+    // update the balance
+    balanceOf[_to] += _value;
+
+    allowance[_from][msg.sender] -=_value;
+    // transfer event
+    emit Transfer(_from,_to,_value);
+    // return a boolean
+    return true;
   }
 }
